9faf3dfaa45cd3bd08b633b2405bfd653dd57929
Phillecia Qualls <plqualls@msn.com>
2021-03-23 21:41:31 -0500

    updated files

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,4 +1,4 @@
-//Store API inside query
+//Store API inside query.
 var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 //Perform a Get request to the query and send the data.features object to the createFeatures function.
@@ -12,9 +12,31 @@ function onEachFeature(feature, layer) {
     layer.bindPopup("<h3>" + feature.properties.place + "</h3><hr><p>" + new Date(feature.properties.time) + "<p>");
 }
 
-//Create the circle radius based on the magnitude
+//Create the circle radius based on the magnitude.
 function radiusSize(magnitude) {
     return magnitude * 20000;
+}
+
+//Set the circle color based on the magnitude.
+function circleColor(magnitude) {
+    if (magnitude < 1) {
+        return "#ccff33"
+    }
+    else if (magnitude < 2) {
+        return "#ffff33"
+    }
+    else if (magnitude < 3) {
+        return "#ffcc33"
+    }
+    else if (magnitude < 4) {
+        return "#ff9933"
+    }
+    else if (magnitude < 5) {
+        return "#ff6633"
+    }
+    else {
+        return "ff3333"
+    }
 }
 
 
